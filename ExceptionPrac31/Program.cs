2e2ad00e07f8ef53f50f83c268e890879af5aca6
Gihun Ham <ham@e-mirim.hs.kr>
2023-08-24 14:33:28 +0900

    finally 구문 적용하기

--- ExceptionPrac31/Program.cs
+++ ExceptionPrac31/Program.cs
@@ -18,14 +18,23 @@ namespace ExceptionPrac31
                 int input = int.Parse(Console.ReadLine());
                 Console.WriteLine("입력한 위치의 값은 '"
                         + array[input] + "'입니다.");
-            }catch(IndexOutOfRangeException ex){
-                Console.WriteLine("인덱스 범위를 벗어났습니다!");            
-            }catch(FormatException ex){
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("인덱스 범위를 벗어났습니다!");
+            }
+            catch (FormatException ex)
+            {
                 Console.WriteLine("숫자를 입력해주세요!");
-            }catch(Exception ex){
+            }
+            catch (Exception ex)
+            {
                 Console.WriteLine(ex.GetType());
                 Console.WriteLine(ex.Message);
                 Console.WriteLine(ex.StackTrace);
+            }
+            finally {
+                Console.WriteLine("옳게 실행되든 예외가 발생하든 실행됨");
             }
                        
         }
